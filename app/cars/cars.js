342623dd3d95df6cbbc68b5e64bf766444d6f234
Ricardo Piña Arellano <ricardo.pina@globant.com>
2016-01-28 09:33:42 -0600

    feat(Locals): add translation files

--- app/cars/cars.js
+++ app/cars/cars.js
@@ -3,14 +3,20 @@
 
   var cars;
 
-  cars = angular.module('cars', ['carsConstants', 'carsDirectives', 'carsFactories', 'carsFilters', 'carsServices'])
+  cars = angular.module('cars', [
+    'carsConstants',
+    'carsDirectives',
+    'carsFactories',
+    'carsFilters',
+    'carsServices'
+  ])
     .config(configProviders);
 
   function configProviders($controllerProvider, $provide, $urlRouterProvider) {
-    app.controller = $controllerProvider.register;
-    app.directive = $compileProvider.directive;
-    app.factory = $provide.factory;
-    app.service = $provide.service;
+    cars.controller = $controllerProvider.register;
+    cars.directive = $compileProvider.directive;
+    cars.factory = $provide.factory;
+    cars.service = $provide.service;
 
     $urlRouterProvider.otherwise('/');
   }

fc96e7b7cb5d2a74e0f24d01caaf96e1e4bbd25d
Ricardo Pina Arellano <ricardo.pina@global.com>
2016-02-03 18:08:53 -0600

    refactor(Tasks): override the test task used the new karma version

--- tasks/test.js
+++ tasks/test.js
@@ -2,25 +2,19 @@
 
 var KARMA_ROUTE_FILE = config.path.join('./', 'karma.conf.js');
 
-var runKarma = function (configFilePath, options, cb) {
-  configFilePath = config.path.resolve(configFilePath);
+var karma = function karma(done) {
+  var karmaConfigFile = config.path.resolve(KARMA_ROUTE_FILE);
 
-  var colors = config.gutil.colors;
-  var configuration = config.karmaConfig(configFilePath, options);
-  var log = config.gutil.log;
-  var server = config.karma.server;
-
-  server.start(configuration, function (exitCode) {
-    log('Karma has exited with ' + colors.red(exitCode));
-    cb();
+  new config.karma.Server({
+    browsers: [process.env.KARMA_BROWSER],
+    configFile: karmaConfigFile,
+    singleRun: process.env.KARMA_RUN !== 'false'
+  })
+  .start(karmaConfigFile, function (exitCode) {
+    config.gutil.log('Karma has exited with ' + colors.red(exitCode));
+    done();
     process.exit(exitCode);
   });
 };
 
-config.gulp.task('karma', function (done) {
-
-  runKarma(KARMA_ROUTE_FILE, {
-    browsers: [process.env.KARMA_BROWSER],
-    singleRun: process.env.KARMA_RUN !== 'false'
-  }, done);
-});
+config.gulp.task('karma', karma);
